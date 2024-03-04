a94af9e6f068adad03ec20ed581238c87c03d804
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2024-03-04 20:52:49 -0300

    Adding fixed Q-Learning with heatmap

--- QLearning.py
+++ QLearning.py
@@ -1,11 +1,12 @@
 import numpy as np
+import gym
 import random
 from numpy import savetxt
 import sys
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 #
-# This class implements the Q-Learning algorithm.
+# This class implements the Q Learning algorithm.
 # We can use this implementation to solve Toy text environments from Gym project. 
 #
 
@@ -20,49 +21,80 @@ class QLearning:
         self.epsilon_min = epsilon_min
         self.epsilon_dec = epsilon_dec
         self.episodes = episodes
-        self.actions_per_episode = []
+        self.q_table_memo = []
 
     def select_action(self, state):
         rv = random.uniform(0, 1)
         if rv < self.epsilon:
-            return self.env.action_space.sample() 
-        return np.argmax(self.q_table[state]) 
+            return self.env.action_space.sample() # Explore action space
+        return np.argmax(self.q_table[state]) # Exploit learned values
 
     def train(self, filename, plotFile):
         actions_per_episode = []
         for i in range(1, self.episodes+1):
-            (state, _) = self.env.reset()
-            reward = 0
+            state, _ = self.env.reset()
+            rewards = 0
             done = False
             actions = 0
-            rewards = 0
 
             while not done:
                 action = self.select_action(state)
                 next_state, reward, done, truncated, _ = self.env.step(action) 
+        
+                # Adjust Q value for current state
                 old_value = self.q_table[state, action]
-                next_max =  np.max(self.q_table[next_state, :]) 
-                new_value = old_value + self.alpha*(reward+ self.gamma*next_max - old_value) 
+                next_max = np.max(self.q_table[next_state])
+                new_value = old_value + self.alpha * (reward + self.gamma * next_max - old_value)
                 self.q_table[state, action] = new_value
+                
                 state = next_state
-                actions=actions+1
-                rewards+= reward
+                actions += 1
+                rewards += reward
 
-            actions_per_episode.append(rewards)
             if i % 100 == 0:
-                sys.stdout.write("Episodes: " + str(i) +'\r')
-                sys.stdout.flush()
+               actions_per_episode.append(actions)
+               sys.stdout.write("Episodes: " + str(i) +'\r')
+               sys.stdout.flush()
             
             if self.epsilon > self.epsilon_min:
                 self.epsilon = self.epsilon * self.epsilon_dec
+            
+            if i == 1 or i == 5000 or i == self.episodes:
+                self.q_table_memo.append(self.q_table.copy())
 
         savetxt(filename, self.q_table, delimiter=',')
-        return self.q_table, actions_per_episode
+        if (plotFile is not None): self.plotactions(plotFile, actions_per_episode)
+        return self.q_table
 
     def plotactions(self, plotFile, actions_per_episode):
         plt.plot(actions_per_episode)
         plt.xlabel('Episodes')
         plt.ylabel('# Actions')
         plt.title('# Actions vs Episodes')
+        plt.legend()
         plt.savefig(plotFile+".jpg")     
         plt.close()
+        self.plot_q_table()
+
+    def update(self, state, action, next_state, reward, done):
+        old_value = self.q_table[state, action]
+        next_max = np.max(self.q_table[next_state])
+        new_value = old_value + self.alpha * (reward + self.gamma * next_max - old_value)
+        self.q_table[state, action] = new_value
+
+    def plot_q_table(self):
+        if len(self.q_table_memo) >= 3:
+            fig, ax = plt.subplots(ncols=3)
+            for i in range(3):
+                ax[i] = sns.heatmap(self.q_table_memo[i][0: 8, :], ax=ax[i], cmap="BuPu")
+                ax[i].set_xlabel("Actions")
+                ax[i].set_ylabel("States")
+            ax[0].set_title("Q-Table Initial")
+            ax[1].set_title("Q-Table Middle")
+            ax[2].set_title("Q-Table Final")
+            plt.tight_layout()
+            plt.savefig("results/q-table-heatmap.jpg", dpi=300)
+            plt.close()
+        else:
+            print("Not enough Q-table snapshots for visualization.")
+    
--- TaxiDriverGym.py
+++ TaxiDriverGym.py
@@ -1,114 +1,32 @@
 import random
 from IPython.display import clear_output
-import gymnasium as gym
+import gym
 import numpy as np
 from QLearning import QLearning
 from numpy import loadtxt
+from time import sleep
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 env = gym.make("Taxi-v3", render_mode='ansi').env
 
-def mean(action):
-    out = []
-    a = list(np.arange(0,len(action),10))
-    for i in range(1,len(a)):
-        out.append(np.mean(action[a[i-1]:a[i]]))
-    return out
-
-a, g, e = 0.1, 0.1, 0.7
-qlearn = QLearning(env, alpha=a, gamma=g, epsilon=e, epsilon_min=0.05, epsilon_dec=0.99, episodes=5000)
-q_table, action = qlearn.train('data/q-table-taxi-driver.csv', 'results/actions_taxidriver2')
-action = mean(action)
-plt.plot(range(len(action)), action, label="alpha = " + str(a) + " : gamma= " + str(g) + " : epsilon=" + str(e))
-
-a, g, e = 0.5, 0.5, 0.7
-qlearn = QLearning(env, alpha=a, gamma=g, epsilon=e, epsilon_min=0.05, epsilon_dec=0.99, episodes=5000)
-q_table, action = qlearn.train('data/q-table-taxi-driver.csv', 'results/actions_taxidriver2')
-action = mean(action)
-plt.plot(range(len(action)), action, label="alpha = " + str(a) + " : gamma= " + str(g) + " : epsilon=" + str(e))
-
-a, g, e = 0.9, 0.9, 0.99
-qlearn = QLearning(env, alpha=a, gamma=g, epsilon=e, epsilon_min=0.05, epsilon_dec=0.99, episodes=5000)
-q_table, action = qlearn.train('data/q-table-taxi-driver.csv', 'results/actions_taxidriver2')
-action = mean(action)
-plt.plot(range(len(action)), action, label="alpha = " + str(a) + " : gamma= " + str(g) + " : epsilon=" + str(e))
-
-plt.xlim(0, 100)
-plt.xlabel('Episodes')
-plt.ylabel('# Rewards')
-plt.title('# Rewards vs Episodes')
-plt.legend(loc="best")
-plt.savefig("./results/actions_taxidriver"+".jpg")     
-plt.close()
-
-
-'''
-estados = [0, 2500, 4999]  # Estados inicial, intermediário e final
-
-# Plotagem dos heatmaps para os estados escolhidos
-for i, estado in enumerate(estados):
-    plt.imshow(q_table, cmap='viridis', interpolation='nearest', aspect='auto')
-    plt.title(f'Q-Table - Estado {i+1}')
-    plt.xlabel('Ações')
-    plt.ylabel('Estados')
-    plt.colorbar()
-    plt.savefig(f'./results/q_table_estado_{i+1}.jpg')
-    plt.close()
-
-
-# Choose specific states to visualize the evolution of the Q-table
-initial_state = 0  # Choose an initial state
-intermediate_state = 50  # Choose an intermediate state
-final_state = 99  # Choose a final state
-
-# Extract the Q-tables corresponding to the chosen states
-q_tables = [q_table1, q_table2, q_table3]
-chosen_states = [initial_state, intermediate_state, final_state]
-
-# Plot the heatmaps of the chosen Q-tables
-plt.figure(figsize=(15, 5))
-for i, (q_table, state) in enumerate(zip(q_tables, chosen_states), 1):
-    plt.subplot(1, 3, i)
-    sns.heatmap(q_table[state].reshape((1, -1)), cmap='hot', annot=True, cbar=False)
-    plt.title(f"State {state}")
-plt.tight_layout()
-plt.savefig("./results/q_table_evolution_heatmaps.jpg")
-plt.close()
-'''
-env = gym.make("Taxi-v3", render_mode='ansi').env
-
-# apenas execute as próximas linhas se você deseja treinar o agente novamente
-
-alpha_values = [0.5, 0.3, 0.7]
-gamma_values = [0.9, 0.95, 0.05]
-epsilon_values = [0.7, 0.8, 0.9]
-
-actions_per_episode = []
-
-for a, g, e in alpha_values, gamma_values:
-    print(a,g,e)
-    qlearn = QLearning(env, alpha=a, gamma=g, epsilon=e, epsilon_min=0.05, epsilon_dec=0.99, episodes=50000)
-    q_table, action_per_episode = qlearn.train('data/q-table-taxi-driver.csv', 'results/actions_taxidriver')
-    actions_per_episode.append(action_per_episode)
-    
-qlearn.plotactions("./results/plotFile", action_per_episode)
+# only execute the following lines if you want to create a new q-table
+qlearn = QLearning(env, alpha=0.1, gamma=0.9, epsilon=0.7, epsilon_min=0.05, epsilon_dec=0.99, episodes=10000)
+q_table = qlearn.train('data/q-table-taxi-driver.csv', 'results/actions_taxidriver')
 #q_table = loadtxt('data/q-table-taxi-driver.csv', delimiter=',')
 
-
-(state, _) = env.reset()
+state, _ = env.reset()
 epochs, penalties, reward = 0, 0, 0
 done = False
 frames = [] # for animation
-
     
-while (not done) and (epochs < 100):
+while not done and epochs < 100:
     action = np.argmax(q_table[state])
-    state, reward, done, t, info = env.step(action)
+    state, reward, done, truncated, info = env.step(action)
 
     if reward == -10:
         penalties += 1
 
+    # Put each rendered frame into dict for animation
     frames.append({
         'frame': env.render(),
         'state': state,
@@ -117,9 +35,6 @@ while (not done) and (epochs < 100):
         }
     )
     epochs += 1
-
-from IPython.display import clear_output
-from time import sleep
 
 clear_output(wait=True)
 
@@ -133,8 +48,9 @@ def print_frames(frames):
         print(f"Action: {frame['action']}")
         print(f"Reward: {frame['reward']}")
         sleep(.1)
-
+        
 print_frames(frames)
+
 print("\n")
 print("Timesteps taken: {}".format(epochs))
 print("Penalties incurred: {}".format(penalties))
